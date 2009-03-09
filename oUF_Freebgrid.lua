fcb48081ac408a6ee0dbb89828ba9b50fca368d0
Heath Ishee <ishee.heath@gmail.com>
2009-03-09 09:52:35 -0500

    0.2

--- oUF_Freebgrid.lua
+++ oUF_Freebgrid.lua
@@ -1,23 +1,199 @@
-local texture = "Interface\\AddOns\\oUF_Freebgrid\\media\\gradient"
-local hightlight = "Interface\\AddOns\\oUF_Freebgrid\\media\\white"
-local font = "Interface\\AddOns\\oUF_Freebgrid\\media\\ABF.ttf"
-local fontsize = 12
+local layoutName = "Freebgrid"
+local layoutPath = "Interface\\Addons\\oUF_"..layoutName
+local mediaPath = layoutPath.."\\media\\"
 
-local height, width = 35, 35
+local texture = mediaPath.."gradient"
+local hightlight = mediaPath.."white"
 
--- Vertical health?
-local verticalhp = true
--- Debuff icon size
-local iconsize = 20
--- Filter debuffs by your class?
-local filterdebuff = true
+local font,fontsize = mediaPath.."CalibriBold.ttf",12			-- The font and fontSize for Names and Health
+local symbols, symbolsSize = mediaPath.."PIZZADUDEBULLETS.ttf", 12	-- The font and fontSize for TagEvents
+local symbols1, symbols1Size = mediaPath.."STYLBCC_.ttf", 12		-- The font and fontSize for TagEvents
+
+local height, width = 40, 40
+local playerClass = select(2, UnitClass("player"))
+
+local highlight = true		-- MouseOver Highlight?
+local filterdebuff = true	-- Filter debuffs by your class?(oUF_DebuffHighlight)
+local indicators = false		-- Class Indicators?
+
+local vertical = true 		-- Vertical bars?
+local manabars = false		-- Mana Bars?
+local Licon = false		-- Leader icon?
+local ricon = false		-- Raid icon?
+
+local numberize = function(val)
+	if(val >= 1e4) then
+        return ("%.1fk"):format(val / 1e3)
+	elseif (val >= 1e6) then
+		return ("%.1fm"):format(val / 1e6)
+	else
+		return val
+	end
+end
 
 local backdrop = {
 	bgFile = [=[Interface\ChatFrame\ChatFrameBackground]=], tile = true, tileSize = 16,
 	insets = {top = -2, left = -2, bottom = -2, right = -2},
 }
 
-local colors = {
+--=========================------------- Big Thanks to jadakren! 
+
+oUF.pomCount = {"i","h","g","f","Z","Y"}
+oUF.unitStyle={
+	["player"] = function()
+	end,
+	["target"] = function()
+	end,
+	["targettarget"] = function()
+	end,
+	["focus"] = function()
+	end,
+	["focustarget"] = function()
+	end,
+	["pet"] = function()
+	end,
+	["pettarget"] = function()
+	end
+}
+oUF.classIndicators={
+		["DRUID"] = {
+				["TL"] = "[tree]",
+				["TR"] = "[gotw]",
+				["BL"] = "[lb]",
+				["BR"] = "[rejuv][regrow][flour]"
+		},
+		["PRIEST"] = {
+				["TL"] = "[pws][ws]",
+				["TR"] = "[sp][fort][fw]",
+				["BL"] = "[pom]",
+				["BR"] = "[rnw][gotn]"
+		},
+		["PALADIN"] = {
+			["TL"] = "",
+			["TR"] = "",
+			["BL"] = "",
+			["BR"] = ""
+		},
+		["WARLOCK"] = {
+			["TL"] = "",
+			["TR"] = "",
+			["BL"] = "",
+			["BR"] = ""
+		},
+		["ROGUE"] = {
+			["TL"] = "",
+			["TR"] = "",
+			["BL"] = "",
+			["BR"] = ""
+		}
+		
+	}
+oUF.TagEvents["[shortName]"] = "UNIT_NAME_UPDATE"
+oUF.Tags["[shortName]"] = function(u) 
+	return string.sub(UnitName(u),1,4) or '' 
+end
+
+oUF.TagEvents["[raidhp]"] = "UNIT_HEALTH UNIT_MAXHEALTH"
+oUF.Tags["[raidhp]"] = function(u)
+	o = ""
+	if not(u == nil) then
+		local c, m, n= UnitHealth(u), UnitHealthMax(u), UnitName(u)
+		if(c <= 1) then 
+			o = "DEAD" 
+		elseif not UnitIsConnected(u) then
+			return "D/C" 
+		elseif(c >= m) then 
+			o = n:utf8sub(1,4)
+		elseif(UnitCanAttack("player", u)) then
+			o = math.floor(c/m*100+0.5).."%"
+		else
+		 	o = "-"..numberize(m - c)
+		end
+	end
+	return o
+end
+oUF.Tags["[pom]"] = function(u) local c = select(4, UnitAura(u, "Prayer of Mending")) return c and "|cffFFCF7F"..oUF.pomCount[c].."|r" or "" end
+oUF.TagEvents["[pom]"] = "UNIT_AURA"
+
+oUF.Tags["[gotn]"] = function(u) return UnitAura(u, "Gift of the Naaru") and "|cff33FF33.|r" or "" end
+oUF.TagEvents["[gotn]"] = "UNIT_AURA"
+oUF.Tags["[rnw]"] = function(u) return UnitAura(u, "Renew") and "|cff33FF33.|r" or "" end
+oUF.TagEvents["[rnw]"] = "UNIT_AURA"
+
+oUF.Tags["[pws]"] = function(u) return UnitAura(u, "Power Word: Shield") and "|cff33FF33.|r" or "" end
+oUF.TagEvents["[pws]"] = "UNIT_AURA"
+oUF.Tags["[ws]"] = function(u) return UnitDebuff(u, "Weakened Soul") and "|cffFF5500.|r" or "" end
+oUF.TagEvents["[ws]"] = "UNIT_AURA"
+
+
+oUF.Tags["[sp]"] = function(u) return (UnitAura(u, "Prayer of Shadow Protection") or UnitAura(u, "Shadow Protection")) and "" or "|cff9900FF.|r" end
+oUF.TagEvents["[sp]"] = "UNIT_AURA"
+oUF.Tags["[fort]"] = function(u) return (UnitAura(u, "Prayer of Fortitude") or UnitAura(u, "Power Word: Fortitude")) and "" or "|cff00A1DE.|r" end
+oUF.TagEvents["[fort]"] = "UNIT_AURA"
+oUF.Tags["[fw]"] = function(u) return UnitAura(u, "Fear Ward") and "|cffCA21FF.|r" or "" end
+oUF.TagEvents["[fw]"] = "UNIT_AURA"
+
+--druid
+
+oUF.Tags["[lb]"] = function(u) local c = select(4, UnitAura(u, "Lifebloom")) return c and "|cffA7FD0A"..c.."|r" or "" end
+oUF.Tags["[rejuv]"] = function(u) return UnitAura(u, "Rejuvenation") and "|cff00FEBF.|r" or "" end
+oUF.Tags["[regrow]"] = function(u) return UnitAura(u, "Regrowth") and "|cff00FF10.|r" or "" end
+oUF.Tags["[flour]"] = function(u) return UnitAura(u, "Flourish") and "|cff33FF33.|r" or "" end
+oUF.Tags["[tree]"] = function(u) return UnitAura(u, "Tree of Life") and "|cff33FF33.|r" or "" end
+oUF.Tags["[gotw]"] = function(u) return UnitAura(u, "Gift of the Wild") and "|cff33FF33.|r" or "" end
+oUF.TagEvents["[lb]"] = "UNIT_AURA"
+oUF.TagEvents["[rejuv]"] = "UNIT_AURA"
+oUF.TagEvents["[regrow]"] = "UNIT_AURA"
+oUF.TagEvents["[flour]"] = "UNIT_AURA"
+oUF.TagEvents["[tree]"] = "UNIT_AURA"
+oUF.TagEvents["[gotw]"] = "UNIT_AURA"
+
+
+local function applyAuraIndicator(self)
+--========= ----- =========--
+		self.AuraStatusTL = self.Health:CreateFontString(nil, "OVERLAY")
+		self.AuraStatusTL:ClearAllPoints()
+		self.AuraStatusTL:SetPoint("TOPLEFT", self.Health, "TOPLEFT", 0, 18)
+		self.AuraStatusTL:SetJustifyH("LEFT")
+		self.AuraStatusTL:SetFont(font, 22, "OUTLINE")
+		self:Tag(self.AuraStatusTL, oUF.classIndicators[playerClass]["TL"])
+	
+		self.AuraStatusTR = self.Health:CreateFontString(nil, "OVERLAY")
+		self.AuraStatusTR:ClearAllPoints()
+		self.AuraStatusTR:SetPoint("TOPRIGHT", self.Health, "TOPRIGHT", 5, 18)
+		self.AuraStatusTR:SetJustifyH("RIGHT")
+		self.AuraStatusTR:SetFont(font, 22, "OUTLINE")
+		self:Tag(self.AuraStatusTR, oUF.classIndicators[playerClass]["TR"])
+
+		self.AuraStatusBR1 = self.Health:CreateFontString(nil, "OVERLAY")
+		self.AuraStatusBR1:ClearAllPoints()
+		self.AuraStatusBR1:SetPoint("BOTTOMRIGHT", self.Health, "BOTTOMRIGHT", 5, -5)
+		self.AuraStatusBR1:SetJustifyH("RIGHT")
+		self.AuraStatusBR1:SetFont(symbols, symbolsSize, "OUTLINE")
+		self:Tag(self.AuraStatusBR1, oUF.classIndicators[playerClass]["BL"])	
+
+		self.AuraStatusBR2 = self.Health:CreateFontString(nil, "OVERLAY")
+		self.AuraStatusBR2:ClearAllPoints()
+		self.AuraStatusBR2:SetPoint("BOTTOMRIGHT", self.Health, "BOTTOMRIGHT", 5, -5)
+		self.AuraStatusBR2:SetJustifyH("RIGHT")
+		self.AuraStatusBR2:SetFont(font, 22, "OUTLINE")
+		self:Tag(self.AuraStatusBR2, oUF.classIndicators[playerClass]["BR"])	
+	--========= ----- =========--
+end
+
+
+--===========================--
+local colors = setmetatable({
+	power = setmetatable({
+		['MANA'] = {.31,.45,.63},
+		['RAGE'] = {.69,.31,.31},
+		['FOCUS'] = {.71,.43,.27},
+		['ENERGY'] = {.65,.63,.35},
+		['RUNIC_POWER'] = {0,.8,.9},
+	}, {__index = oUF.colors.power}),
+}, {__index = oUF.colors})
+
+local colorsC = {
 	class ={
 		["DEATHKNIGHT"] = { 0.77, 0.12, 0.23 },
 		["DRUID"] = { 1.0 , 0.49, 0.04 },
@@ -31,16 +207,15 @@ local colors = {
 		["WARRIOR"] = { 0.78, 0.61, 0.43 },
 	},
 }
-setmetatable(colors.class, {
+setmetatable(colorsC.class, {
 	__index = function(self, key)
 		return { 0.78, 0.61, 0.43 }
 	end
 })
 
 local GetClassColor = function(unit)
-	return unpack(colors.class[select(2, UnitClass(unit))])
+	return unpack(colorsC.class[select(2, UnitClass(unit))])
 end
-
 local menu = function(self)
 	local unit = self.unit:sub(1, -2)
 	local cunit = self.unit:gsub("(.)", string.upper, 1)
@@ -52,34 +227,20 @@ local menu = function(self)
 	end
 end
 
-
 -- Health Function
 local updateHealth = function(self, event, unit, bar, min, max)
+
 	if(max ~= 0)then
-	  r,g,b = self.ColorGradient((min/max), .9,.1,.1, .8,.8,.1, 1,1,1)
+	  r,g,b = self.ColorGradient((min/max), .9,.1,.1, .9,.1,.1, 1,1,1)
 	end
 
 	bar.value:SetTextColor(r,g,b)
 
-	local perc = floor(min/max*100)
-	if(not UnitIsConnected(unit)) then
-		bar:SetValue(0)
-		bar.value:SetText('|cffD7BEA5'..'Off')
-	elseif(UnitIsDead(unit)) then
-		bar.value:SetText('|cffD7BEA5'..'Dead')
-	elseif(UnitIsGhost(unit)) then
-		bar.value:SetText('|cffD7BEA5'..'Ghost')
-        elseif(perc > 90) then
-		bar.value:SetText(UnitName(unit):sub(1, 3))
-	else
-		bar.value:SetFormattedText("-%0.1f",math.floor((max-min)/100)/10)
-	end
 	if UnitIsDeadOrGhost(unit) or not UnitIsConnected(unit) then
 		bar.bg:SetVertexColor(0.3, 0.3, 0.3)
 	else
 		bar.bg:SetVertexColor(GetClassColor(unit))
 	end
-	
 end
 
 local OnEnter = function(self)
@@ -95,57 +256,114 @@ end
 -- Style
 local func = function(self, unit)
 	self.menu = menu
-
+	self.colors = colors
+	
 	self:EnableMouse(true)
-
 	self:SetScript("OnEnter", OnEnter)
 	self:SetScript("OnLeave", OnLeave)
-
 	self:RegisterForClicks"anyup"
 	self:SetAttribute("*type2", "menu")
+
 	-- Health
 	local hp = CreateFrame"StatusBar"
-	hp:SetHeight(height)
 	hp:SetStatusBarTexture(texture)
-	if(verticalhp)then
-	  hp:SetOrientation("VERTICAL")
-	end
 	hp:SetStatusBarColor(0, 0, 0)
 	hp:SetAlpha(0.8)
+	hp.colorHappiness = true
 	hp.frequentUpdates = true
-
-	hp:SetParent(self)
-	hp:SetPoint"TOPLEFT"
-	hp:SetPoint"BOTTOMRIGHT"
+	if(manabars)then
+	  if(vertical)then
+	    hp:SetWidth(width*.95)
+	    hp:SetOrientation("VERTICAL")
+	    hp:SetParent(self)
+	    hp:SetPoint"TOP"
+	    hp:SetPoint"BOTTOM"
+	    hp:SetPoint"LEFT"
+  	  else
+	    hp:SetHeight(height*.95)
+	    hp:SetParent(self)
+	    hp:SetPoint"LEFT"
+	    hp:SetPoint"RIGHT"
+	    hp:SetPoint"TOP"
+	  end
+  	else
+	  if(vertical)then
+	    hp:SetWidth(width)
+	    hp:SetOrientation("VERTICAL")
+	    hp:SetParent(self)
+	    hp:SetPoint"TOPLEFT"
+	    hp:SetPoint"BOTTOMRIGHT"
+  	  else
+	    hp:SetParent(self)
+	    hp:SetPoint"TOPLEFT"
+	    hp:SetPoint"BOTTOMRIGHT"
+	  end
+	end
 
 	local hpbg = hp:CreateTexture(nil, "BORDER")
 	hpbg:SetAllPoints(hp)
 	hpbg:SetTexture(texture)
+	hpbg.colorClass = true
 
 	self:SetBackdrop(backdrop)
-	self:SetBackdropColor(0, 0, 0, .8)
+	self:SetBackdropColor(0, 0, 0, .5)
 
 	-- Health Text
 	local hpp = hp:CreateFontString(nil, "OVERLAY")
-	hpp:SetFont(font, fontsize)
+	hpp:SetFont(font, fontsize, "THINOUTLINE")
 	hpp:SetShadowOffset(1,-1)
 	hpp:SetPoint("CENTER")
 	hpp:SetJustifyH("CENTER")
+
+	self:Tag(hpp, "[raidhp]")
 
 	hp.bg = hpbg
 	hp.value = hpp
 	self.Health = hp
 	self.OverrideUpdateHealth = updateHealth
 
-	-- Hightlight
-	local hl = hp:CreateTexture(nil, "OVERLAY")
-	hl:SetAllPoints(self)
-	hl:SetTexture(hightlight)
-	hl:SetVertexColor(1,1,1,.2)
-	hl:SetBlendMode("ADD")
-	hl:Hide()
+	if(manabars)then
+	  local pp = CreateFrame"StatusBar"
+	  pp:SetStatusBarTexture(texture)
+	  pp.colorPower = true
+	  pp.frequentUpdates = true
+	
+	  if(vertical)then
+	    pp:SetWidth(width*.05)
+	    pp:SetOrientation("VERTICAL")
+	    pp:SetParent(self)
+	    pp:SetPoint"TOP"
+	    pp:SetPoint"BOTTOM"
+	    pp:SetPoint"RIGHT"
+  	  else
+	    pp:SetHeight(height*.05)
+	    pp:SetParent(self)
+	    pp:SetPoint"LEFT"
+	    pp:SetPoint"RIGHT"
+	    pp:SetPoint"BOTTOM"
+	  end
 
-	self.Highlight = hl
+	  local ppbg = pp:CreateTexture(nil, "BORDER")
+	  ppbg:SetAllPoints(pp)
+	  ppbg:SetTexture(texture)
+	  ppbg.multiplier = .3
+	  pp.bg = ppbg
+
+	  self.Power = pp
+	  self.PostUpdatePower = updatePower
+	end
+
+	-- Hightlight
+	if(highlight)then
+	  local hl = hp:CreateTexture(nil, "OVERLAY")
+	  hl:SetAllPoints(self)
+	  hl:SetTexture(hightlight)
+	  hl:SetVertexColor(1,1,1,.1)
+	  hl:SetBlendMode("ADD")
+	  hl:Hide()
+
+	  self.Highlight = hl
+	end
 
 	-- DebuffHidghtlight
 	self.DebuffHighlightBackdrop = true
@@ -155,11 +373,40 @@ local func = function(self, unit)
 	  self.DebuffHighlightFilter = true
 	end
 
+	-- Range Alpha
 	if(not unit) then
 		self.Range = true
 		self.inRangeAlpha = 1
-		self.outsideRangeAlpha = .4
+		self.outsideRangeAlpha = .5
 	end
+
+--==========--
+--  ICONS   --
+--==========--
+--Leader Icon
+	if(Licon)then
+	self.Leader = self.Health:CreateTexture(nil, "OVERLAY")
+	self.Leader:SetPoint("TOPLEFT", self, 0, 8)
+	self.Leader:SetHeight(16)
+	self.Leader:SetWidth(16)
+	end
+
+-- Raid Icon
+	if(ricon)then
+	self.RaidIcon = self.Health:CreateTexture(nil, "OVERLAY")
+	self.RaidIcon:SetPoint("TOP", self, 0, 8)
+	self.RaidIcon:SetHeight(16)
+	self.RaidIcon:SetWidth(16)
+	end
+	
+	self.ReadyCheck = self.Health:CreateTexture(nil, "OVERLAY")
+	self.ReadyCheck:SetPoint("TOPRIGHT", self, 0, 8)
+	self.ReadyCheck:SetHeight(16)
+	self.ReadyCheck:SetWidth(16)
+	
+	if(indictors)then
+	  applyAuraIndicator(self)
+  	end
 
 	self:SetAttribute('initial-height', height)
 	self:SetAttribute('initial-width', width)
@@ -169,16 +416,16 @@ end
 
 oUF:RegisterStyle("Freebgrid", func)
 
-oUF:SetActiveStyle"Freebgrid"
+oUF:SetActiveStyle"Freebgrid"  
 
 local raid = {}
-for i = 1, 5 do
+for i = 1, 8 do
 	local group = oUF:Spawn('header', 'oUF_Group'..i)
 	group:SetManyAttributes('groupFilter', tostring(i), 'showRaid', true, 'yOffset', -4)
 	table.insert(raid, group)
 	if(i == 1) then
 		group:SetManyAttributes('showParty', true, 'showPlayer', true)
-		group:SetPoint('TOPLEFT', UIParent, 5, -550)
+		group:SetPoint('TOPLEFT', UIParent, 5, -500)
 	else
 		group:SetPoint('TOPLEFT', raid[i-1], 'TOPRIGHT', 4, 0)
 	end
---
+++ oUF_HealComm.lua
@@ -0,0 +1,168 @@
+--==============================================================================
+--
+-- oUF_HealComm
+--
+-- Uses data from LibHealComm-3.0 to add incoming heal estimate bars onto units 
+-- health bars.
+--
+-- * currently won't update the frame if max HP is unknown (ie, restricted to 
+--   players/pets in your group that are in range), hides the bar for these
+-- * can define frame.ignoreHealComm in layout to not have the bars appear on 
+--   that frame
+--
+--=============================================================================
+oUF.debug=false
+local oufDebug = function(msg) 
+	if(oUF.debug) then	print("|cff00FF10"..msg.."|r") end
+end
+if not oUF then return end
+
+--set texture and color here
+local color = {
+    r = 0,
+    g = 1,
+    b = 0,
+    a = .5,
+}
+
+local oUF_HealComm = {}
+
+local healcomm = LibStub("LibHealComm-3.0")
+
+local playerName = UnitName("player")
+local playerIsCasting = false
+local playerHeals = 0
+local playerTarget = ""
+
+--update a specific bar
+local updateHealCommBar = function(frame, unit)
+    local curHP = UnitHealth(unit)
+    local maxHP = UnitHealthMax(unit)
+    local percHP = curHP / maxHP
+
+    local incHeals = select(2, healcomm:UnitIncomingHealGet(unit, GetTime())) or 0
+
+    --add player's own heals if casting on this unit
+    if playerIsCasting then
+        for i = 1, select("#", playerTarget) do
+            local target = select(i, playerTarget)
+            if target == unit then
+                incHeals = incHeals + playerHeals
+            end
+        end
+    end
+
+    --hide if unknown max hp or no heals inc
+    if maxHP == 100 or incHeals == 0 then
+        frame.HealCommBar:Hide()
+        return
+    else
+        frame.HealCommBar:Show()
+    end
+
+    percInc = incHeals / maxHP
+    h = frame.Health:GetHeight()
+    w = frame.Health:GetWidth()
+    orient = frame.Health:GetOrientation()
+	oufDebug("orient: "..orient, "h: "..h, "w: "..w, "percInc: "..percInc)
+    
+    frame.HealCommBar:ClearAllPoints()
+    frame.HealCommBar:SetFrameStrata("TOOLTIP")
+    if(orient=="VERTICAL")then
+		frame.HealCommBar:SetHeight(percInc * h)
+		frame.HealCommBar:SetWidth(w)
+		frame.HealCommBar:SetPoint("BOTTOM", frame.Health, "BOTTOM", 0, h * percHP)
+	else
+		frame.HealCommBar:SetWidth(percInc * w)
+		frame.HealCommBar:SetHeight(h)
+		frame.HealCommBar:SetPoint("LEFT", frame.Health, "LEFT", w * percHP,0)
+	end
+end
+
+--used by library callbacks, arguments should be list of units to update
+local updateHealCommBars = function(...)
+	for i = 1, select("#", ...) do
+		local unit = select(i, ...)
+
+        --search current oUF frames for this unit
+        for frame in pairs(oUF.units) do
+            local name, server = UnitName(frame)
+            if server then name = strjoin("-",name,server) end
+            if name == unit and not oUF.units[frame].ignoreHealComm then
+                updateHealCommBar(oUF.units[frame],unit)
+            end
+        end
+	end
+end
+
+local function hook(frame)
+	if frame.ignoreHealComm then return end
+	
+    --create heal bar here and set initial values
+    if(frame.Health:GetOrientation() =="VERTICAL")then
+		local hcb = CreateFrame"StatusBar"
+		hcb:SetWidth(frame.Health:GetWidth()) -- same height as health bar
+		hcb:SetHeight(4) --no initial width
+		hcb:SetStatusBarTexture(frame.Health:GetStatusBarTexture():GetTexture())
+		hcb:SetStatusBarColor(color.r, color.g, color.b, color.a)
+		hcb:SetParent(frame)
+		hcb:SetPoint("BOTTOM", frame.Health, "TOP",0,0) --attach to immediate right of health bar to start
+	--    hcb:SetFrameLevel
+		hcb:Hide() --hide it for now
+		frame.HealCommBar = hcb
+	else
+		local hcb = CreateFrame"StatusBar"
+		hcb:SetHeight(frame.Health:GetHeight()) -- same height as health bar
+		hcb:SetWidth(4) --no initial width
+		hcb:SetStatusBarTexture(frame.Health:GetStatusBarTexture():GetTexture())
+		hcb:SetStatusBarColor(color.r, color.g, color.b, color.a)
+		hcb:SetParent(frame)
+		hcb:SetPoint("LEFT", frame.Health, "RIGHT",0,0) --attach to immediate right of health bar to start
+	--    hcb:SetFrameLevel
+		hcb:Hide() --hide it for now
+		frame.HealCommBar = hcb
+	end
+	local o = frame.PostUpdateHealth
+	frame.PostUpdateHealth = function(...)
+		if o then o(...) end
+        local name, server = UnitName(frame.unit)
+        if server then name = strjoin("-",name,server) end
+        updateHealCommBar(frame, name) --update the bar when unit's health is updated
+	end
+end
+
+--hook into all existing frames
+for i, frame in ipairs(oUF.objects) do hook(frame) end
+
+--hook into new frames as they're created
+oUF:RegisterInitCallback(hook)
+
+--set up LibHealComm callbacks
+function oUF_HealComm:HealComm_DirectHealStart(event, healerName, healSize, endTime, ...)
+	if healerName == playerName then
+		playerIsCasting = true
+		playerTarget = ... 
+		playerHeals = healSize
+	end
+    updateHealCommBars(...)
+end
+
+function oUF_HealComm:HealComm_DirectHealUpdate(event, healerName, healSize, endTime, ...)
+    updateHealCommBars(...)
+end
+
+function oUF_HealComm:HealComm_DirectHealStop(event, healerName, healSize, succeeded, ...)
+    if healerName == playerName then
+        playerIsCasting = false
+    end
+    updateHealCommBars(...)
+end
+
+function oUF_HealComm:HealComm_HealModifierUpdate(event, unit, targetName, healModifier)
+    updateHealCommBars(unit)
+end
+
+healcomm.RegisterCallback(oUF_HealComm, "HealComm_DirectHealStart")
+healcomm.RegisterCallback(oUF_HealComm, "HealComm_DirectHealUpdate")
+healcomm.RegisterCallback(oUF_HealComm, "HealComm_DirectHealStop")
+healcomm.RegisterCallback(oUF_HealComm, "HealComm_HealModifierUpdate")
