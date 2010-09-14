4d81b3ee9eb0f330f69173fd3c7a7d15931b7fd4
freebaser <ishee.heath@gmail.com>
2010-09-14 15:58:57 -0500

    gitignore, temp remove oUF, clean out debuffs.lua

--- raid_debuffs.lua
+++ raid_debuffs.lua
@@ -1,41 +1,18 @@
 
 FreebgridDebuffs = {
-	debuffs = {
-		-- Any Zone
-		[GetSpellInfo(3034)] = 12, -- Viper Sting
+   debuffs = {
+      -- Any Zone
+      [GetSpellInfo(47486)] = 8, -- Mortal Strike
+   },
 
-		[GetSpellInfo(57978)] = 9, -- Wound Poison
-		[GetSpellInfo(47486)] = 8, -- Mortal Strike
-		[GetSpellInfo(46911)] = 8, -- Furious Attacks
-		[GetSpellInfo(49050)] = 8, -- Aimed Shot
+   buffs = {
+      --[GetSpellInfo(871)] = 15, -- Shield Wall
+   },
 
-		[GetSpellInfo(2139)] = 10, -- Counterspell
-
-		[GetSpellInfo(2094)] = 10, -- Blind
-		[GetSpellInfo(33786)] = 10, -- Cyclone
-
-		[GetSpellInfo(12826)] = 7, -- Polymorph
-
-		[GetSpellInfo(53308)] = 7, -- Entangling Roots
-		[GetSpellInfo(14311)] = 7, -- Freezing Trap
-
-		[GetSpellInfo(3775)] = 6, -- Crippling Poison
-		[GetSpellInfo(1715)] = 5, -- Hamstring
-		[GetSpellInfo(2974)] = 5, -- Wing Clip
-
-		[GetSpellInfo(6215)] = 3, -- Fear
-		[GetSpellInfo(10890)] = 3, -- Psychic Scream
-		[GetSpellInfo(17928)] = 3, -- Howl of Terror
-	},
-	buffs = {
-		--[GetSpellInfo(871)] = 15, -- Shield Wall
-	},
-	instances = {
-		--[[
-		["Zone"] = {
-			[Name or GetSpellInfo(#)] = PRIORITY,
-		},
-		]]--
-		
-	},
+   instances = {
+      --["Zone"] = {
+      --	[Name or GetSpellInfo(#)] = PRIORITY,
+      --},
+      
+   },
 }
