a3b77fe5675cadf042c4a14e78bfdd006531d8fb
freebaser <ishee.heath@gmail.com>
2010-12-31 18:16:46 -0600

    out of range arrows

--- elements/range.lua
+++ elements/range.lua
@@ -1,20 +1,88 @@
 local parent, ns = ...
 local oUF = ns.oUF or oUF
 
+-- oUF range element with code sniplets from TomTom
+
 local _FRAMES = {}
 local OnRangeFrame
 
 local UnitInRange, UnitIsConnected = UnitInRange, UnitIsConnected
+local GetPlayerMapPosition, GetPlayerFacing = GetPlayerMapPosition, GetPlayerFacing
 
-local classSpells = {
-    --["PRIEST"] = GetSpellInfo(2050),
-    --["DRUID"] = GetSpellInfo(5185),
-    --["PALADIN"] = GetSpellInfo(635),
-    --["SHAMAN"] = GetSpellInfo(331),
-    --["MAGE"] = GetSpellInfo(475),
-}
+local twopi = math.pi * 2
+local atan2 = math.atan2
 
--- updating of range.
+local function ColorGradient(perc, ...)
+    local num = select("#", ...)
+    local hexes = type(select(1, ...)) == "string"
+
+    if perc == 1 then
+        return select(num-2, ...), select(num-1, ...), select(num, ...)
+    end
+
+    num = num / 3
+
+    local segment, relperc = math.modf(perc*(num-1))
+    local r1, g1, b1, r2, g2, b2
+    r1, g1, b1 = select((segment*3)+1, ...), select((segment*3)+2, ...), select((segment*3)+3, ...)
+    r2, g2, b2 = select((segment*3)+4, ...), select((segment*3)+5, ...), select((segment*3)+6, ...)
+
+    if not r2 or not g2 or not b2 then
+        return r1, g1, b1
+    else
+        return r1 + (r2-r1)*relperc,
+        g1 + (g2-g1)*relperc,
+        b1 + (b2-b1)*relperc
+    end
+end
+
+local function getCoords(column, row)
+    local xstart = (column * 56) / 512
+    local ystart = (row * 42) / 512
+    local xend = ((column + 1) * 56) / 512
+    local yend = ((row + 1) * 42) / 512
+    return xstart, xend, ystart, yend
+end
+
+local texcoords = setmetatable({}, {__index = function(t, k)
+    local col,row = k:match("(%d+):(%d+)")
+    col,row = tonumber(col), tonumber(row)
+    local obj = {getCoords(col, row)}
+    rawset(t, k, obj)
+    return obj
+end})
+
+local function ColorTexture(texture, angle)
+    local perc = math.abs((math.pi - math.abs(angle)) / math.pi)
+
+    local gr,gg,gb = 0, 1, 0
+    local mr,mg,mb = 1, 1, 0
+    local br,bg,bb = 1, 0, 0
+    local r,g,b = ColorGradient(perc, br, bg, bb, mr, mg, mb, gr, gg, gb)		
+    texture:SetVertexColor(r,g,b)
+end
+
+local function RotateTexture(texture, angle)
+    local cell = floor(angle / twopi * 108 + 0.5) % 108
+    local column = cell % 9
+    local row = floor(cell / 9)
+
+    local key = column .. ":" .. row
+    texture:SetTexCoord(unpack(texcoords[key]))
+    texture:Show()
+
+    ColorTexture(texture, angle)
+end
+
+local function GetBearing(unit)
+    local tx, ty = GetPlayerMapPosition(unit)
+    if tx == 0 and ty == 0 then
+        return 999
+    end
+    local px, py = GetPlayerMapPosition("player")
+    return -GetPlayerFacing() - atan2(tx - px, py - ty)
+end
+
 local timer = 0
 local OnRangeUpdate = function(self, elapsed)
     timer = timer + elapsed
@@ -23,22 +91,21 @@ local OnRangeUpdate = function(self, elapsed)
         for _, object in next, _FRAMES do
             if(object:IsShown()) then
                 local range = object.freebRange
-                if(self.class) then
-                    if(UnitIsConnected(object.unit) and IsSpellInRange(self.class, object.unit) == 0) then
-                        if(object:GetAlpha() == range.insideAlpha) then
-                            object:SetAlpha(range.outsideAlpha)
-                        end
-                    elseif(object:GetAlpha() ~= range.insideAlpha) then
-                        object:SetAlpha(range.insideAlpha)
+                if(UnitIsConnected(object.unit) and not UnitInRange(object.unit)) then
+                    if(object:GetAlpha() == range.insideAlpha) then
+                        object:SetAlpha(range.outsideAlpha)
                     end
-                else
-                    if(UnitIsConnected(object.unit) and not UnitInRange(object.unit)) then
-                        if(object:GetAlpha() == range.insideAlpha) then
-                            object:SetAlpha(range.outsideAlpha)
-                        end
-                    elseif(object:GetAlpha() ~= range.insideAlpha) then
-                        object:SetAlpha(range.insideAlpha)
+                    local bearing = GetBearing(object.unit)
+                    if bearing == 999 then
+                        object.freebarrow:Hide()
+                        return
                     end
+
+                    RotateTexture(object.freebarrow, bearing)
+
+                elseif(object:GetAlpha() ~= range.insideAlpha) then
+                    object:SetAlpha(range.insideAlpha)
+                    object.freebarrow:Hide()
                 end
             end
         end
@@ -52,14 +119,20 @@ local Enable = function(self)
     if(range and range.insideAlpha and range.outsideAlpha) then
         table.insert(_FRAMES, self)
 
-        local _, class = UnitClass("player")
         if(not OnRangeFrame) then
             OnRangeFrame = CreateFrame"Frame"
-            OnRangeFrame.class = classSpells[class]
             OnRangeFrame:SetScript("OnUpdate", OnRangeUpdate)
         end
-
         OnRangeFrame:Show()
+
+        local frame = CreateFrame"Frame"
+        frame:SetAllPoints(self)
+        frame:SetFrameLevel(6)
+        self.freebarrow = frame:CreateTexture(nil, "OVERLAY")
+        self.freebarrow:SetTexture"Interface\\Addons\\oUF_Freebgrid\\Media\\Arrow"
+        self.freebarrow:SetPoint("TOPRIGHT", frame, "TOPRIGHT")
+        self.freebarrow:SetSize(16, 16)
+        self.freebarrow:Hide()       
     end
 end
 
